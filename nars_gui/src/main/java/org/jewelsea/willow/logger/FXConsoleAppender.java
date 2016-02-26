6d60c7fc7011068e1d1f583fe71f36c8e5fffc2c
SeH <seh999@gmail.com>
2016-02-25 19:37:26 -0500

    fx log viewer in progress

---
+++ nars_gui/src/main/java/org/jewelsea/willow/logger/FXConsoleAppender.java
@@ -0,0 +1,62 @@
+package org.jewelsea.willow.logger;
+
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+
+import java.io.IOException;
+
+abstract public class FXConsoleAppender extends AppenderBase<ILoggingEvent> {
+  static int DEFAULT_LIMIT = 10;
+  int counter = 0;
+  int limit = DEFAULT_LIMIT;
+  
+  //PatternLayoutEncoder encoder;
+  
+  public void setLimit(int limit) {
+    this.limit = limit;
+  }
+
+  public int getLimit() {
+    return limit;
+  }
+  
+  @Override
+  public void start() {
+//    if (this.encoder == null) {
+//      addError("No encoder set for the appender named ["+ name +"].");
+//      return;
+//    }
+//
+//    try {
+//      encoder.init(System.out);
+//    } catch (IOException e) {
+//    }
+    super.start();
+  }
+
+  abstract public void append(ILoggingEvent event);
+
+//    if (counter >= limit) {
+//      return;
+//    }
+//    // output the events as formatted by our layout
+//
+//
+//    try {
+//      this.encoder.doEncode(event);
+//    } catch (IOException e) {
+//    }
+//
+//    // prepare for next event
+//    counter++;
+
+
+//  public PatternLayoutEncoder getEncoder() {
+//    return encoder;
+//  }
+//
+//  public void setEncoder(PatternLayoutEncoder encoder) {
+//    this.encoder = encoder;
+//  }
+}
