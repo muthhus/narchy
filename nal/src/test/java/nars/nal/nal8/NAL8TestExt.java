5fbe403348eedb8dc15ad195e92ab340f392bfbc
SeH <seh999@gmail.com>
2016-04-03 19:45:01 -0400

    sjfajsd

--- nal/src/test/java/nars/nal/nal8/NAL8TestExt.java
+++ nal/src/test/java/nars/nal/nal8/NAL8TestExt.java
@@ -28,32 +28,33 @@ public class NAL8TestExt extends AbstractNALTest {
 
 
 
-    @Test
-    public void subsent_1_simpler()  {
-        test()
-
-                //.log()
-
-                .input("hold:t2. :|:") //@ 0
-                //.inputAt(5, "at:t1. :|:")
-                .inputAt(10, "(hold:t2 &&+5 (at:t1 &&+5 (open(t1) &&+5 [opened]:t1))).")
-                //.inputAt(15, "[opened]:t1. :|:")
-
-                .mustBelieve(cycles*8, "open(t1)",
-                        //1.0f, 0.81f,
-                        //-5);
-                        1.0f, 0.34f,
-                        10);
-
-    }
+//    @Test
+//    public void subsent_1_simpler()  {
+//        test()
+//
+//                //.log()
+//
+//                .input("hold:t2. :|:") //@ 0
+//                //.inputAt(5, "at:t1. :|:")
+//                .inputAt(10, "(hold:t2 &&+5 (at:t1 &&+5 (open(t1) &&+5 [opened]:t1))).")
+//                //.inputAt(15, "[opened]:t1. :|:")
+//
+//                .mustBelieve(cycles*8, "open(t1)",
+//                        //1.0f, 0.81f,
+//                        //-5);
+//                        1.0f, 0.34f,
+//                        10);
+//
+//    }
 
     @Test public void subsent_1_even_simpler()  {
-        int time = cycles * 16;
+        int time = cycles * 4;
         test()
                 //.log()
                 .input("at:t1. :|:") //@ 0
                 .inputAt(10, "(at:t1 &&+5 (open(t1) &&+5 [opened]:t1)).")
-                .mustBelieve(time, "open(t1)", 1.0f, 0.73f, 5)
+                //.mustBelieve(time, "open(t1)", 1.0f, 0.73f, 5)
+                .mustBelieve(time, "(open(t1) &&+5 [opened]:t1)", 1.0f, 0.81f, 5)
                 .mustNotOutput(time, "open(t1)", '.', 1f, 1f, 0.59f, 0.59f, 5) //detect cyclic decomposition
                 .mustNotOutput(time, "open(t1)", '.', 1f, 1f, 0.32f, 0.32f, 5) //detect cyclic decomposition
         ;
@@ -64,19 +65,11 @@ public class NAL8TestExt extends AbstractNALTest {
                 //.log()
                 .input("at:t1. :|:") //@ 0
                 .inputAt(10, "(at:t1 &&+5 (open(t1) &&+5 [opened]:t1))!")
-                .mustDesire(time, "open(t1)", 1.0f, 0.73f, 5)
+                .mustDesire(time, "(open(t1) &&+5 [opened]:t1)", 1.0f, 0.81f, 5)
+                //.mustDesire(time, "open(t1)", 1.0f, 0.73f, 5)
         ;
     }
-    @Test public void arbitraryConjunction() {
-        test()
-                .believe("believe:(x).") //psuedo operators since operation is > nal6
-                .believe("want:(x).") //psuedo operators since operation is > nal6
-                .believe("((believe:($1) && want:($1)) ==> grateful:($1))")
-                //.mustBelieve(cycles*3, "(believe:(x) && want:(x)).", 1.00f, 0.81f)
-                .mustBelieve(cycles*3, "grateful:(x).", 1.00f, 0.73f);
 
-
-    }
     @Test
     public void subsent_simultaneous()  {
         TestNAR tester = test();
