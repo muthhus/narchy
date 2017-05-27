d39689c070e137602b82ee57a6d0702c35cbe761
SeH <seh1seh@yandex.com>
2017-05-26 23:29:05 -0400

    objenome

--- nal/src/test/java/nars/budget/TestDerivationBudgeting.java
+++ nal/src/test/java/nars/budget/TestDerivationBudgeting.java
@@ -1,33 +1,55 @@
 package nars.budget;
 
 import jcog.O;
+import jcog.random.XORShiftRandom;
+import jcog.random.XorShift128PlusRandom;
 import nars.nar.Default;
 import nars.time.CycleTime;
 import nars.util.exe.BufferedSynchronousExecutor;
+import org.jetbrains.annotations.Nullable;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 
 public class TestDerivationBudgeting {
 
     @Test
-    public void testDerivationStealsParentBudget() {
+    public void testWonderAboutIncompletePlan() {
 
         O o = O.of(
             BufferedSynchronousExecutor.class,
             CycleTime.class,
             new Default.DefaultTermIndex(1024)
         );
-        System.out.println(o.toString());
+        System.out.println(o);
 
 
-        Default x = o.a(Default.class);
-        assertNotNull(x);
+        O.How<Default> h = o.how(Default.class);
+        System.out.println(h);
+        assertNotNull(h);
 
+        assertEquals(1, h.wonder.size());
 
+        @Nullable Default d = h.get();
 
-        //at the right speed to ensure that "trains" of thought can form through time (relative to forgetting rates, etc)
     }
 
+    @Test
+    public void testComplete() {
+        O o = O.of(
+                BufferedSynchronousExecutor.class,
+                CycleTime.class,
+                new Default.DefaultTermIndex(1024),
+                new XorShift128PlusRandom(1)
+        );
+        System.out.println(o);
+
+        System.out.println();
+
+        O.How<Default> h = o.how(Default.class);
+        System.out.println(h);
+
+    }
 }
--- util/src/main/java/jcog/O.java
+++ util/src/main/java/jcog/O.java
@@ -6,11 +6,19 @@ import com.google.common.graph.Graphs;
 import com.google.common.graph.MutableGraph;
 import jcog.list.FasterList;
 import org.apache.commons.lang3.ClassUtils;
+import org.eclipse.collections.api.tuple.Pair;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Parameter;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Consumer;
+
+import static java.util.stream.Collectors.toList;
+import static org.eclipse.collections.impl.tuple.Tuples.pair;
 
 /**
  * objenome v3 prototype
@@ -50,11 +58,13 @@ public class O {
         return o;
     }
 
-    /** integrates information about 'x' into this */
+    /**
+     * integrates information about 'x' into this
+     */
     protected void learn(Object x) {
         assert (x != this);
         if (x instanceof Class) {
-            includeA((Class) x);
+            this.learnClass((Class) x, null);
         } else if (x instanceof O) {
             O o = (O) x;
             how.nodes().addAll(o.how.nodes());
@@ -65,63 +75,157 @@ public class O {
             the.add(x); //allow it as an instance too
         } else {
             if (the.add(x)) {
-                includeA(x.getClass());
+                this.learn(x.getClass());
             }
         }
     }
 
-    private void includeA(Class c) {
-        if (c.isInterface() || c.isPrimitive())
-            throw new UnsupportedOperationException("classes currently must be concrete implementations");
+    private <X> void learnClass(Class<X> c, @Nullable Consumer<Pair<Constructor<X>, Parameter>> wonder) {
+        if (c.isInterface() || c.isPrimitive() || Modifier.isAbstract(c.getModifiers()))
+            return; //throw new UnsupportedOperationException("classes currently must be concrete implementations");
 
-        if (a.contains(c))
-            return; //already mapped
+        boolean novel = a.add(c);
+
+        if (!novel && wonder == null)
+            return; //nothing to do with this already learned type at this time
+
+        how.addNode(c);
 
         nextConstructor:
         for (Constructor cc : c.getConstructors()) {
             if (!Modifier.isPublic(cc.getModifiers()))
                 continue;
 
-
             Parameter[] cp = cc.getParameters();
             List<Parameter> primitives = new FasterList();
+            List<Class> instances = new FasterList();
             if (cp.length > 0) {
                 //determine if some permutation of the vocabulary fits
                 for (Parameter p : cp) {
                     Class pt = p.getType();
                     if (pt.isPrimitive()) {
                         primitives.add(p);
-                    } else if (!a.contains(pt))
-                        break nextConstructor;
+                    } else {
+                        //if (!how.nodes().add(pt))
+                        if (a.contains(pt))
+                            instances.add(pt);
+                        else {
+                            if (!a.contains(pt)) {
+                                if (wonder != null) {
+                                    wonder.accept(pair(cc, p));
+                                }
+                                break nextConstructor;
+                            }
+                        }
+                    }
                 }
             }
 
-            how.addNode(cc);
-            how.addNode(c);
-            how.putEdge(cc, c);
 
-            for (Parameter p : primitives) {
-                how.addNode(p);
-                how.putEdge(p, cc);
+            if (how.addNode(cc)) {
+                how.putEdge(cc, c);
+
+                for (Parameter p : primitives) {
+                    how.addNode(p);
+                    how.putEdge(p, cc);
+                    if (wonder!=null && !is.containsKey(p))
+                        wonder.accept(pair(cc,p));
+                }
+                for (Class i : instances) {
+                    how.putEdge(i, cc);
+                    learnClass(i, wonder);
+                }
             }
+
+
         }
         //ConstructorUtils.getMatchingAccessibleConstructor(c)
         //ConstructorUtils.invokeConstructor()
         //ClassUtils.
 
-        this.a.add(c);
-        ClassUtils.hierarchy(c, ClassUtils.Interfaces.INCLUDE).forEach(this.a::add);
+        if (novel) {
+            how.addNode(c);
+
+//            ClassUtils.hierarchy(c, ClassUtils.Interfaces.EXCLUDE).forEach(s -> {
+//                if (s!=c && s!=Object.class) {
+//                    a.add(s);
+//                    how.putEdge(c, s); //assignable from
+//                }
+//            });
+        }
     }
 
 
-    public <X> X a(Class<X> c) {
+//    public <X> X a(Class<X> c) {
+//        return a(c, null);
+//    }
 
-        Set<Object> what = Graphs.reachableNodes(how, c);
-        MutableGraph<Object> can = Graphs.inducedSubgraph(how, what);
+    public <X> How<X> how(Class<X> c) {
+        return new How(c, this);
+    }
 
-        System.out.println(can);
+    public static class How<X> { //implements Iterable<X> {
+        public final Set<Pair<Constructor<X>, Parameter>> wonder = new HashSet();
+        public final MutableGraph<Object> plan;
+        private final Class<X> what;
 
-        return null;
+        public How(Class<X> what, O o) {
+            this.what = what;
+
+            o.learnClass(what, wonder::add);
+
+            //((ConfigurableMutableGraph) o.how).predecessors(what)
+
+            Set<Object> before = Graphs.reachableNodes(o.how, what);
+            this.plan = Graphs.inducedSubgraph(o.how, before);
+        }
+
+        /** returns the first successfully constructed possibility */
+        @Nullable public X get(/* strategy */) {
+            return get(what);
+        }
+
+        @Nullable public <Y> Y get(Class<Y> y/* Strategy... */) {
+            //first layer priors
+            Set<Object> before = plan.successors(y);
+            if (before.isEmpty())
+                return null;
+
+            //Strategy 1. choose from the instances if available
+            List instances = before.stream()
+                    .filter(p -> y.isAssignableFrom(p.getClass()))
+                    .collect(toList());
+            if (!instances.isEmpty())
+                return (Y) instances.get(ThreadLocalRandom.current().nextInt(instances.size()));
+
+            //Strategy 2. choose by invoking any constructors, simplest and least exceptions first
+            List constructors = before.stream()
+                    .filter(z -> z instanceof Constructor)
+                    .filter(c -> y.isAssignableFrom( ((Constructor)c).getDeclaringClass() ) )
+                    //.sorted(x -> x.) //TODO
+                    .collect(toList());
+            if (!constructors.isEmpty()) {
+                Constructor c = (Constructor) constructors.get(0);
+            }
+
+            return null;
+
+        }
+
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "plan=" + Joiner.on("\n\t\t").join(plan.edges()) +
+                    "\nwonder=" + Joiner.on("\n\t\t").join(wonder) +
+                    '}';
+        }
+
+        /*@NotNull
+        @Override
+        public Iterator<X> iterator() {
+            return null;
+        }*/
     }
 
 
@@ -132,7 +236,7 @@ public class O {
                 "\n\t, the=" + the +
                 "\n\t, a=" + a +
                 "\n\t, is=" + is +
-                "\n\t, build=" + Joiner.on("\n\t\t").join(how.edges()) +
+                "\n\t, how=" + Joiner.on("\n\t\t").join(how.edges()) +
                 '}';
     }
 
