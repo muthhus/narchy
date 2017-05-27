9a99b3ff62f350d2d97de5827063972b7742b226
SeH <seh1seh@yandex.com>
2017-05-27 05:15:43 -0400

    objenome

--- nal/src/test/java/nars/budget/TestDerivationBudgeting.java
+++ nal/src/test/java/nars/budget/TestDerivationBudgeting.java
@@ -1,8 +1,9 @@
 package nars.budget;
 
 import jcog.O;
-import jcog.decide.Deciding;
 import jcog.random.XorShift128PlusRandom;
+import nars.conceptualize.DefaultConceptBuilder;
+import nars.index.term.NullTermIndex;
 import nars.nar.Default;
 import nars.time.CycleTime;
 import nars.util.exe.BufferedSynchronousExecutor;
@@ -10,6 +11,7 @@ import org.junit.Test;
 
 import java.lang.reflect.Parameter;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.Assert.assertEquals;
@@ -28,11 +30,6 @@ public class TestDerivationBudgeting {
         );
         System.out.println(o);
 
-
-        O.Possible<Default> h = o.possible(Default.class);
-        System.out.println(h);
-        assertNotNull(h);
-
         //assertEquals(1, h.unknown.size());
 
         //@Nullable Default d = h.get();
@@ -41,36 +38,43 @@ public class TestDerivationBudgeting {
 
     @Test
     public void testComplete() {
-        O o = O.of(
+
+        O of = O.of(
                 BufferedSynchronousExecutor.class,
                 CycleTime.class,
+
                 new Default.DefaultTermIndex(1024),
-                new XorShift128PlusRandom(1)
+                new NullTermIndex(new DefaultConceptBuilder()),
+
+                new XorShift128PlusRandom(1),
+                Random.class
+
         );
+        for (int i = 0; i < 100; i++) {
+            Default d = of.a(Default.class, new O.How<>() {
 
-
-        O.Possible<Default> h = o.possible(Default.class);
-        System.out.println(h);
-
-        Default d = o.get(Default.class, new O.How() {
-
-            @Override
-            public int which(List options) {
-                return ThreadLocalRandom.current().nextInt(options.size()); //random
-            }
-
-            @Override
-            public Object value(Parameter inConstructor) {
-
-                Class<?> tt = inConstructor.getType();
-                System.out.println(inConstructor + " " + tt);
-                if (tt == int.class) {
-                    return Integer.valueOf(1);
+                @Override
+                public int impl(List choose) {
+                    return ThreadLocalRandom.current().nextInt(choose.size()); //random
                 }
-                return null;
-            }
-        });
-        //h.get();
+
+                @Override
+                public Object value(Parameter inConstructor) {
+
+                    Class<?> tt = inConstructor.getType();
+                    //System.out.println(inConstructor + " " + tt);
+                    if (tt == int.class) {
+                        return 1;
+                    } else if (tt == float.class) {
+                        return 0.5f;
+                    } else if (tt == long.class) {
+                        return 1L;
+                    }
+                    return null;
+                }
+            });
+            System.out.println();
+        }
 
     }
 }
--- util/src/main/java/jcog/O.java
+++ util/src/main/java/jcog/O.java
@@ -7,19 +7,61 @@ import com.google.common.graph.MutableGraph;
 import com.google.common.primitives.Primitives;
 import jcog.list.FasterList;
 import org.apache.commons.lang3.ClassUtils;
+import org.eclipse.collections.api.tuple.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import javax.lang.model.type.PrimitiveType;
+import java.io.Serializable;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Parameter;
 import java.util.*;
 
+import static org.eclipse.collections.impl.tuple.Tuples.pair;
+
 /**
  * objenome v3 prototype
+ * <p>
+ * container instance
+ * <p>
+ * --configured with:
+ * >=0 specified implementation classes ('a')
+ * <p>
+ * constructor injection
+ * <p>
+ * >=0 specified singleton-ish instances ('the')
+ * <p>
+ * >=0 parent containers to inherit from (TODO)
+ * <p>
+ * --reflectively generates an object dataflow graph
+ * for permutable instantiation that detects and allows dynamic non-deterministic construction choices
+ * <p>
+ * choice of available implementing classes to be instantiated (recursively)
+ * <p>
+ * choice of per-constructor parameter values (ie. primitives)
+ * <p>
+ * wrapper proxies (TODO)
+ * <p>
+ * state saving (TODO)
+ * <p>
+ * numerical and evolutionary optimization (TODO)
+ * <p>
+ * quantifiable metrics and unit tests
+ * <p>
+ * catalog parameter spaces and their performance
  */
 public class O {
+
+    public static O of(Object... xx) {
+        return new O(xx);
+    }
+
+    @Nullable
+    public <X> X a(@NotNull Class<X> c, @NotNull How<X> h) {
+        Possible<X> p = possible(c);
+        Map m = p.permute(h);
+        return (m != null) ? p.build(c, h.finish(c, m)) : null;
+    }
 
     /**
      * parents inherited
@@ -38,21 +80,20 @@ public class O {
      */
     final Set<Class> a = new HashSet<>();
 
-    /**
-     * constant property values, named according to the requiree
-     */
-    final Map<String, Class> is = new HashMap();
+//    /**
+//     * constant property values, named according to the requiree
+//     */
+//    final Map<String, Class> is = new HashMap();
 
 
     final MutableGraph<Object> how = GraphBuilder.
             directed().allowsSelfLoops(false).build();
 
-    public static O of(Object... xx) throws RuntimeException {
-        O o = new O();
+    protected O(Object... xx) {
         for (Object x : xx)
-            o.learn(x);
-        return o;
+            learn(x);
     }
+
 
     /**
      * integrates information about 'x' into this
@@ -61,12 +102,12 @@ public class O {
         assert (x != this);
         if (x instanceof Class) {
             Class xx = (Class) x;
-            if (xx.isInterface() || xx.isPrimitive() || Modifier.isAbstract(xx.getModifiers()))
+            if (xx.isInterface() || xx.isPrimitive() || Modifier.isAbstract(xx.getModifiers()) || !nonSys(xx))
                 throw new UnsupportedOperationException("a specified implementation must be concrete: " + xx);
             if (a.add(xx))
                 learnClass(xx);
         } else if (x instanceof O) {
-            learnAll(x);
+            learnAll((O) x);
         } else {
             learnInstance(x);
         }
@@ -76,7 +117,7 @@ public class O {
         if (the.add(x)) {
             Class<?> c = x.getClass();
             ClassUtils.hierarchy(c, ClassUtils.Interfaces.INCLUDE).forEach(s -> {
-                if (s != c && s != Object.class) {
+                if (nonSys(s)) {
                     how.putEdge(x, s); //superclasses assignable from instance 'x'
                 }
             });
@@ -84,25 +125,27 @@ public class O {
         }
     }
 
-    private void learnAll(Object x) {
-        O o = (O) x;
+    private static boolean nonSys(Class<?> s) {
+         return s != Object.class && s!=Serializable.class; //ETC
+    }
+
+    private void learnAll(O o) {
         how.nodes().addAll(o.how.nodes());
         how.edges().addAll(o.how.edges());
         the.addAll(o.the);
         a.addAll(o.a);
-        is.putAll(o.is);
-        the.add(x); //allow it as an instance too
+        //is.putAll(o.is);
+        the.add(o); //allow it as an instance too
     }
 
     private <X> void learnClass(Class<? extends X> c) {
         if (c.isPrimitive())
             return;
 
-        if (!how.addNode(c))
-            return;
+        how.addNode(c);
 
         ClassUtils.hierarchy(c, ClassUtils.Interfaces.INCLUDE).forEach(s -> {
-            if (s != c && s != Object.class) {
+            if (s != c && nonSys(s)) {
                 how.putEdge(c, s); //superclasses assignable from instance 'x'
             }
         });
@@ -145,7 +188,9 @@ public class O {
 
                     //2. find any specified implementations to assign, and then learn recursively
                     a.stream().filter(i::isAssignableFrom).forEach(x -> {
-                        how.putEdge(x, i);
+                        if (i!=x) {
+                            how.putEdge(x, i);
+                        }
                         learnClass(x);
                     });
 
@@ -168,72 +213,29 @@ public class O {
 //        return a(c, null);
 //    }
 
-    public <X> Possible<X> possible(Class<X> c) {
+    <X> Possible<X> possible(Class<X> c) {
         return new Possible(c, this);
     }
 
-    @Nullable
-    public <X> X get(@NotNull Class<X> c, @NotNull How h) {
-        Possible<X> p = possible(c);
-        if (p == null)
-            return null;
-
-        Map m = new HashMap();
-        if (get(c, h, p, m)) {
-            //use 'm' to construct return value
-            System.out.println(m);
-        }
-        return null;
-    }
-
-    private boolean get(@NotNull Object current, How h, Possible p, Map m) {
-
-        Set<Object> next = p.p.successors(current);
-        int s = next.size();
-        switch (s) {
-            case 0:
-                if (current instanceof Parameter) {
-                    Parameter pc = (Parameter) current;
-                    Class<?> pt = pc.getType();
-                    Object v = h.value(pc);
-                    if (v == null)
-                        return false;
-                    if (pt.isPrimitive() /* or string etc */) {
-                        if (Primitives.unwrap(v.getClass()) != pt)
-                            return false;
-                    } else {
-                        if (!pc.getType().isAssignableFrom(v.getClass()))
-                            throw new ClassCastException();
-                    }
-                    m.put(pc, v);
-                }
-                return true;
-            case 1:
-                return get(next.iterator().next(), h, p, m);
-            default:
-                if (current.getClass() == Class.class) {
-                    //choose one implementation
-                    List nn = Lists.newArrayList(next);
-                    int n = h.which(nn);
-                    if (n < 0 || n >= s)
-                        return false; //invalid choice
-                    return get(nn.get(n), h, p, m);
-                } else {
-                    return next.stream().allMatch(n -> get(n, h, p, m));
-                }
-        }
-    }
 
 
-    public interface How {
-        int which(List options);
+    public interface How<X> {
+
+        int impl(List choose);
 
         Object value(Parameter inConstructor);
+
+        /** any final steps or filters can be applied here prior to instantiations */
+        default Map finish(@NotNull Class<X> c, Map m) {
+            return m;
+        }
     }
 
-    public static class Possible<X> { //implements Iterable<X> {
+    static class Possible<X> { //implements Iterable<X> {
 
-        public final MutableGraph<Object> p = GraphBuilder.directed().allowsSelfLoops(false).build();
+        public final MutableGraph p = GraphBuilder.directed()
+                //.nodeOrder(ElementOrder.<>natural())
+                .allowsSelfLoops(false).build();
 
         private final Class<X> what;
         private final O o;
@@ -246,7 +248,7 @@ public class O {
                     '}';
         }
 
-        public Possible(Class<X> what, O o) {
+        Possible(Class<X> what, O o) {
             this.what = what;
             this.o = o;
 
@@ -260,78 +262,130 @@ public class O {
                 return true; //already solved
 
 
-            boolean foundInstances = false;
+            boolean constructable = false;
             for (Object provided : o.the) {
                 //choose from the instances if available
                 if (what.isAssignableFrom(provided.getClass())) {
                     p.putEdge(what, provided);//, "PROVIDED");
-                    foundInstances = true;
+                    constructable = true;
                 }
             }
-            if (foundInstances)
-                return true;
+//            if (foundInstances)
+//                return true;
 
 
             o.learnClass(what);
-            Set<Object> next = o.how.predecessors(what); //first layer priors
-            if (next.isEmpty())
-                return false;
 
-            boolean constructable = false;
-            for (Object z : next) {
-                if (z instanceof Constructor) {
-                    if (what.isAssignableFrom(((Constructor) z).getDeclaringClass())) {
-                        //choose by invoking any constructors, simplest and least exceptions first
-                        Constructor constructor = (Constructor) z;
+            Collection<Object> next = //clone to avoid concurrent modification exception
+                    Lists.newArrayList( o.how.predecessors(what) ); //first layer priors
+            if (!next.isEmpty()) {
 
-                        boolean foundConstructor = true;
-                        for (Parameter parameter : constructor.getParameters()) {
-                            Class pt = parameter.getType();
-                            if (pt.isPrimitive()) {
-                                p.putEdge(constructor, parameter); //primitive parameter
-                            } else if (!solve(pt)) {
-                                foundConstructor = false;
-                                break;
-                            } else {
-                                //solved
-                                p.putEdge(parameter, pt);
-                                p.putEdge(constructor, parameter); //instance parameter
+                for (Object z : next) {
+                    if (z instanceof Constructor) {
+                        if (what.isAssignableFrom(((Constructor) z).getDeclaringClass())) {
+                            //choose by invoking any constructors, simplest and least exceptions first
+                            Constructor constructor = (Constructor) z;
+
+                            boolean foundConstructor = true;
+                            for (Parameter parameter : constructor.getParameters()) {
+                                Class pt = parameter.getType();
+                                if (pt.isPrimitive()) {
+                                    p.putEdge(constructor, parameter); //primitive parameter
+                                } else if (!solve(pt)) {
+                                    foundConstructor = false;
+                                    break;
+                                } else {
+                                    //solved
+                                    p.putEdge(parameter, pt);
+                                    p.putEdge(constructor, parameter); //instance parameter
+                                }
+                            }
+                            if (foundConstructor) {
+                                p.putEdge(what, constructor); //, "CONSTRUCT");
+                                constructable = true;
                             }
                         }
-                        if (foundConstructor) {
-                            p.putEdge(what, constructor); //, "CONSTRUCT");
+                    } else if (z instanceof Class) {
+                        //assert(z assignable from.. )
+                        //recurse down the type hierarchy
+                        if (solve((Class) z)) {
+                            p.putEdge(what, z);
                             constructable = true;
                         }
-                    }
-                } else if (z instanceof Class) {
-                    //assert(z assignable from.. )
-                    //recurse down the type hierarchy
-                    if (solve((Class) z)) {
-                        p.putEdge(what, z);
-                        constructable = true;
+                    } else {
+                        Class<?> zg = z.getClass();
+                        if (what.isAssignableFrom(zg)) {
+                            if (solve((Class)zg)) {
+                                if (what!=zg)
+                                    p.putEdge(what, zg);
+                                constructable = true;
+                            }
+                        }
                     }
                 }
             }
 
-            if (!constructable)
-                return false;
+            return constructable;
+        }
 
+        public Map permute(@NotNull How h) {
+            Map m = new HashMap();
+            if (!get(what, h, m)) {
+                return null;
+            }
+            return m;
+        }
 
-//            if (!o.how.nodes().contains(what))
-//                return false;
+        protected boolean get(@NotNull Object current, How h, Map m) {
 
+            Set<Object> next = p.successors(current);
+            int s = next.size();
+            switch (s) {
+                case 0:
+                    if (current instanceof Parameter) {
+                        Parameter pc = (Parameter) current;
+                        Class<?> pt = pc.getType();
+                        Object v = h.value(pc);
+                        if (v == null)
+                            v = Void.class;
+                        else if (pt.isPrimitive() /* or string etc */) {
+                            if (Primitives.unwrap(v.getClass()) != pt)
+                                v = Void.class;
+                        } else {
+                            if (!pc.getType().isAssignableFrom(v.getClass()))
+                                throw new ClassCastException();
+                        }
+                        m.put(pc, v);
+                    }
+                    return true;
+                case 1:
+                    return get(next.iterator().next(), h, m);
+                default:
+                    //TODO argument types should be bound in argument,type pairs so they can each have unique assigned impl
+                    if (current.getClass() == Class.class) {
+                        //choose one implementation
+                        List nn = Lists.newArrayList(next);
+                        int n = h.impl(nn);
+                        if (n < 0 || n >= s)
+                            return false; //invalid choice
 
-            return true;
+                        Object impl = nn.get(n);
+                        m.put(current, impl);
+                        return get(impl, h,  m);
+                    } else {
+                        return next.stream().allMatch(n -> get(n, h, m));
+                    }
+            }
+        }
+
+        public <X> X build(@NotNull Class<X> c, Map m) {
+            m.forEach((k,v)->{
+                System.out.println("\t" + k + "\t = " + v);
+            });
+            return null; //TODO
         }
 
 
-
-
-        /*@NotNull
-        @Override
-        public Iterator<X> iterator() {
-            return null;
-        }*/
     }
 
 
@@ -341,7 +395,6 @@ public class O {
                 "\n\tor=" + or +
                 "\n\t, the=" + the +
                 "\n\t, a=" + a +
-                "\n\t, is=" + is +
                 "\n\t, how=" + Joiner.on("\n\t\t").join(how.edges()) +
                 '}';
     }
