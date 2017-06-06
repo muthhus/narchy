b4547ea42b03861135addfe6f2818830f1058392
SeH <seh1seh@yandex.com>
2017-06-06 01:31:49 -0400

    polecart rl

---
+++ lab/src/main/java/nars/experiment/polecart/PoleCart.java
@@ -0,0 +1,300 @@
+package nars.experiment.polecart;
+
+import jcog.Util;
+import nars.*;
+import nars.concept.GoalActionConcept;
+import nars.concept.SensorConcept;
+import org.apache.commons.math3.util.MathUtils;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static jcog.Texts.n2;
+
+/** adapted from: https://github.com/B00075594/CI_Lab2_CartAndPole/blob/master/src/pole.java */
+public class PoleCart extends NAgentX  {
+
+
+    public static void main(String[] arg) {
+        runRT((n) -> {
+
+            try {
+                NAgent a = new PoleCart(n);
+                return a;
+            } catch (Exception e) {
+
+                e.printStackTrace();
+                return null;
+            }
+        }, 20);
+    }
+
+    private static final long serialVersionUID = 1L;
+    private final JPanel panel;
+
+    Thread animatorThread;
+    //next three are for double-buffering
+    Dimension offDimension;
+    Image offImage;
+    Graphics offGraphics;
+
+    // Vars to store current pole and cart position and previous positions
+    double pos, posDot, angle, angleDot;
+
+    float posMin = -2f, posMax = +2f;
+
+    // Constants used for physics
+    public static final double cartMass = 1.;
+    public static final double poleMass = 0.1;
+    public static final double poleLength = 1.;
+    public static final double forceMag = 10.;
+    public static final double tau = 0.02;
+    public static final double fricCart = 0.00005;
+    public static final double fricPole = 0.005;
+    public static final double totalMass = cartMass + poleMass;
+    public static final double halfPole = 0.5 * poleLength;
+    public static final double poleMassLength = halfPole * poleMass;
+    public static final double fourthirds = 4. / 3.;
+
+    float speed = 2f;
+
+
+    // Define the Engine
+    // Define InputVariable1 Theta(t) {angle with perpendicular}
+    SensorConcept inputVariable1;
+    // Define InputVariable1 x(t) {angular velocity}
+    SensorConcept inputVariable2;
+    // OutputVariable {force to be applied}
+    GoalActionConcept outputVariable;
+    // Define the RuleBlock
+    double action;
+
+    public PoleCart(NAR nar) throws Narsese.NarseseException {
+        super("cart", nar);
+
+        this.inputVariable1 = senseNumber("(ang)", () -> MathUtils.normalizeAngle(angle, 0)).resolution(0.1f);
+        this.inputVariable2 = senseNumber("(angDot)", () -> Util.sigmoid(angleDot)).resolution(0.1f);
+        this.outputVariable = actionBipolar($.p("act"), (a) -> {
+            action = a * speed;
+            return a;
+        });
+
+        // Initialise pole state.
+        pos = 0.;
+        posDot = 0.;
+        angle = 0.2; // Pole starts off at an angle 
+        angleDot = 0.;
+        action = 0;
+
+
+        // Set up animation timing.
+        //How many milliseconds between frames?
+
+
+
+        // Handle keyboard events 
+        // LEFT KEY = push the cart left APPLY FORCE FROM RIGHT
+        // RIGHT KEY = push the cart right APPLY FORCE FROM LEFT
+        // SPACE KEY = Reset pole to centre
+//        this.addKeyListener(new KeyAdapter() {
+//            public void keyPressed(KeyEvent e) {
+//                if (e.getKeyCode() == KeyEvent.VK_LEFT)
+//                    action = -1;
+//                else if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+//                    action = 1;
+//                else if (e.getKeyChar() == ' ') {
+//                    action = 0;
+//                    resetPole();
+//                }
+//            }
+//        });
+
+        this.panel = new JPanel(new BorderLayout()) {
+            public Stroke stroke = new BasicStroke(4);
+
+            public void paint(Graphics g) {
+                update(g);
+            }
+
+            public void update(Graphics g) {
+                Dimension d = panel.getSize();
+                Color cartColor = Color.ORANGE;
+                Color arrowColor = Color.WHITE;
+                Color trackColor = Color.GRAY;
+
+                //Create the off-screen graphics context, if no good one exists.
+                if ((offGraphics == null)
+                        || (d.width != offDimension.width)
+                        || (d.height != offDimension.height)) {
+                    offDimension = d;
+                    offImage = panel.createImage(d.width, d.height);
+                    offGraphics = offImage.getGraphics();
+                }
+
+                //Erase the previous image.
+                offGraphics.setColor(new Color(0,0,0,0.5f));
+                offGraphics.fillRect(0, 0, d.width, d.height);
+
+                //Draw Track.
+                double xs[] = {-2.5, 2.5, 2.5, 2.3, 2.3, -2.3, -2.3, -2.5};
+                double ys[] = {-0.4, -0.4, 0., 0., -0.2, -0.2, 0, 0};
+                int pixxs[] = new int[8], pixys[] = new int[8];
+                for (int i = 0; i < 8; i++) {
+                    pixxs[i] = pixX(d, xs[i]);
+                    pixys[i] = pixY(d, ys[i]);
+                }
+                offGraphics.setColor(trackColor);
+                offGraphics.fillPolygon(pixxs, pixys, 8);
+
+                //Draw message
+                // String msg = "Left Mouse Button: push left    Right Mouse Button: push right     Middle Button: PANIC";
+                String msg = "Position = " + n2(pos) + " Angle = " + n2(angle) + " angleDot = " + n2(angleDot);
+                offGraphics.drawString(msg, 20, d.height - 20);
+
+                //Draw cart.
+                offGraphics.setColor(cartColor);
+                offGraphics.fillRect(pixX(d, pos - 0.2), pixY(d, 0), pixDX(d, 0.4), pixDY(d, -0.2));
+
+                ((Graphics2D)offGraphics).setStroke(stroke);
+                //Draw pole.
+                //    offGraphics.setColor(cartColor);
+                offGraphics.drawLine(pixX(d, pos), pixY(d, 0),
+                        pixX(d, pos + Math.sin(angle) * poleLength),
+                        pixY(d, poleLength * Math.cos(angle)));
+
+                //Draw action arrow.
+                if (action != 0) {
+                    int signAction = (action > 0 ? 1 : (action < 0) ? -1 : 0);
+                    int tipx = pixX(d, pos + 0.2 * signAction);
+                    int tipy = pixY(d, -0.1);
+                    offGraphics.setColor(arrowColor);
+                    offGraphics.drawLine(pixX(d, pos), pixY(d, -0.1), tipx, tipy);
+                    offGraphics.drawLine(tipx, tipy, tipx - 4 * signAction, tipy + 4);
+                    offGraphics.drawLine(tipx, tipy, tipx - 4 * signAction, tipy - 4);
+                }
+
+
+                //Last thing: Paint the image onto the screen.
+                g.drawImage(offImage, 0, 0, panel);
+
+            }
+
+        };
+        JFrame f = new JFrame();
+        f.setContentPane(panel);
+        f.setSize(800, 600);
+        f.setVisible(true);
+
+        //start();
+    }
+
+//    public void start() {
+//        //Start animating!
+//        if (animatorThread == null) {
+//            animatorThread = new Thread(this);
+//        }
+//        animatorThread.start();
+//    }
+//
+//    public void stop() {
+//        //Stop the animating thread.
+//        animatorThread = null;
+//        //Get rid of the objects necessary for double buffering.
+//        offGraphics = null;
+//        offImage = null;
+//    }
+
+    @Override
+    protected float act() {
+                    //Update the state of the pole;
+            // First calc derivatives of state variables
+            double force = forceMag * action;
+            //double force = action;
+            double sinangle = Math.sin(angle);
+            double cosangle = Math.cos(angle);
+            double angleDotSq = angleDot * angleDot;
+            double common = (force + poleMassLength * angleDotSq * sinangle
+                    - fricCart * (posDot < 0 ? -1 : 0)) / totalMass;
+            double angleDDot = (9.8 * sinangle - cosangle * common
+                    - fricPole * angleDot / poleMassLength) /
+                    (halfPole * (fourthirds - poleMass * cosangle * cosangle /
+                            totalMass));
+            double posDDot = common - poleMassLength * angleDDot * cosangle /
+                    totalMass;
+
+            //Now update current state.
+            pos += posDot * tau;
+
+            if ((pos >= posMax) || (pos <= posMin)) {
+                //bounce
+                pos = Util.clamp((float) pos, posMin, posMax);
+
+                //posDot = 0;
+                //angleDDot = 0;
+
+                posDot = -0.75f * posDot;
+
+                //posDot = -posDot;
+                //angleDot = -angleDot;
+                //angleDDot = -angleDDot;
+            }
+
+            posDot += posDDot * tau;
+            angle += angleDot * tau;
+            angleDot += angleDDot * tau;
+
+            /**TODO
+             // Above values represent current state of the cart and pole
+             // Control system should take these values and make decision.
+             // We are interested in angle and angleDot;
+             // So we need a function here to set the state of the action for the next
+             // update in time.
+             **/
+
+
+            //Display it.
+            panel.repaint();
+
+//            //Delay depending on how far we are behind.
+//            try {
+//                startTime += delay;
+//                Thread.sleep(Math.max(0,
+//                        startTime - System.currentTimeMillis()));
+//            } catch (InterruptedException e) {
+//                break;
+//            }
+
+        return (float)(1f - Math.abs(MathUtils.normalizeAngle(angle, 0)));
+
+//        System.out.println(angle);
+//        return (float) angleDot;
+    }
+
+
+
+
+    public int pixX(Dimension d, double v) {
+        return (int) Math.round((v + 2.5) / 5.0 * d.width);
+    }
+
+    public int pixY(Dimension d, double v) {
+        return (int) Math.round(d.height - (v + 2.5) / 5.0 * d.height);
+    }
+
+    public int pixDX(Dimension d, double v) {
+        return (int) Math.round(v / 5.0 * d.width);
+    }
+
+    public int pixDY(Dimension d, double v) {
+        return (int) Math.round(-v / 5.0 * d.height);
+    }
+
+    public void resetPole() {
+        pos = 0.;
+        posDot = 0.;
+        angle = 0.;
+        angleDot = 0.;
+    }
+
+
+}
