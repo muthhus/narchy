048b3530c5298b83d5e09ef7caee0824c6d8381f
SeH <seh1seh@yandex.com>
2017-10-13 12:18:21 -0400

    updates

--- run_repl.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd app
-MAVEN_OPTS="-Xmx1000m --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED" \
-    mvn exec:java
