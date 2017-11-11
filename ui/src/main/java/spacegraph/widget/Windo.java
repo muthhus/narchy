f4f482488667a495503ff7a8defa4dd81e1a4a54
SeH <seh1seh@yandex.com>
2017-11-11 14:55:46 -0500

    windo

--- ui/src/main/java/spacegraph/widget/Windo.java
+++ ui/src/main/java/spacegraph/widget/Windo.java
@@ -11,6 +11,7 @@ import spacegraph.render.Draw;
 import spacegraph.widget.button.PushButton;
 
 import static spacegraph.layout.Grid.grid;
+import static spacegraph.widget.Windo.WindowDragMode.MOVE;
 
 /**
  * draggable panel
@@ -19,6 +20,7 @@ public class Windo extends Stacking {
 
     public enum WindowDragMode {
         MOVE,
+        RESIZE_N, RESIZE_E, RESIZE_S, RESIZE_W,
         RESIZE_NW,
         RESIZE_SW,
         RESIZE_NE,
@@ -28,7 +30,7 @@ public class Windo extends Stacking {
     public WindowDragMode dragMode = null;
     public WindowDragMode potentialDragMode = null;
 
-    public final float resizeBorder = 0.05f;
+    public final static float resizeBorder = 0.05f;
 
     private boolean hover;
 //    float pmx, pmy;
@@ -124,7 +126,7 @@ public class Windo extends Stacking {
         if (!moveable())
             return super.onTouch(finger, hitPoint, buttons); //pass-through
 
-        if (dragMode==null && !bounds.contains(finger.hit.x, finger.hit.y)) {
+        if (dragMode == null && !bounds.contains(finger.hit.x, finger.hit.y)) {
             hover = false;
             dragMode = null;
             return null;
@@ -140,37 +142,91 @@ public class Windo extends Stacking {
             if (hitPoint != null) {
 
                 hover = true;
-                if (buttons != null && buttons.length > 0 && buttons[0] == 1) {
-                    if (dragMode == null) {
-                        {
-                            dragMode = WindowDragMode.MOVE;
-                            pmx = x();
-                            pmy = y();
+                float fx = finger.hit.x;
+                float fy = finger.hit.y;
 
-                            //finger.lock(0, )..
+                if (dragMode == null) {
+
+                    potentialDragMode = null;
+
+                    if (potentialDragMode == null && hitPoint.x >= 0.5f - resizeBorder / 2f && hitPoint.x <= 0.5f + resizeBorder / 2) {
+                        if (potentialDragMode == null && hitPoint.y <= resizeBorder) {
+                            potentialDragMode = WindowDragMode.RESIZE_S;
                         }
-                    } else {
-                        switch (dragMode) {
-                            case MOVE:
-
-//                                System.out.println(this + " " + finger.hit);
-                                //System.out.println("\t" + Arrays.toString(finger.hitOnDown));
-                                float tx = pmx + (finger.hit.x - finger.hitOnDown[0].x);
-                                float ty = pmy + (finger.hit.y - finger.hitOnDown[0].y);
-                                pos(tx, ty, w() + tx, h() + ty);
-
-                                break;
+                        if (potentialDragMode == null && hitPoint.y >= 1f - resizeBorder) {
+                            potentialDragMode = WindowDragMode.RESIZE_N;
                         }
                     }
-                    return this;
+                    if (potentialDragMode == null && hitPoint.y >= 0.5f - resizeBorder / 2f && hitPoint.y <= 0.5f + resizeBorder / 2) {
+                        if (potentialDragMode == null && hitPoint.x <= resizeBorder) {
+                            potentialDragMode = WindowDragMode.RESIZE_W;
+                        }
+                        if (potentialDragMode == null && hitPoint.x >= 1f - resizeBorder) {
+                            potentialDragMode = WindowDragMode.RESIZE_E;
+                        }
+                    }
+
+                    if (potentialDragMode == null && hitPoint.x <= resizeBorder) {
+                        if (potentialDragMode == null && hitPoint.y <= resizeBorder) {
+                            potentialDragMode = WindowDragMode.RESIZE_SW;
+                        }
+                        if (potentialDragMode == null && hitPoint.y >= 1f - resizeBorder) {
+                            potentialDragMode = WindowDragMode.RESIZE_NW;
+                        }
+                    }
+
+                    if (potentialDragMode == null && hitPoint.x >= 1f-resizeBorder) {
+
+                        if (potentialDragMode == null && hitPoint.y <= resizeBorder) {
+                            potentialDragMode = WindowDragMode.RESIZE_SE;
+                        }
+                        if (potentialDragMode == null && hitPoint.y >= 1f - resizeBorder) {
+                            potentialDragMode = WindowDragMode.RESIZE_NE;
+                        }
+                    }
+
+
+                    if (potentialDragMode == null)
+                        potentialDragMode = MOVE;
+                }
+
+
+                boolean bDrag = buttons != null && buttons.length > 0 && buttons[0] == 1;
+                if (bDrag) {
+                    if (dragMode == null && potentialDragMode != null) {
+
+                        dragMode = potentialDragMode;
+
+                        //finger.lock(0, )..
+                        pmx = x(); //TODO store these in a shared Finger-context "posOnHit" field, not in this instance
+                        pmy = y();
+
+                    }
+                    if (dragMode != null) {
+                        switch (dragMode) {
+                            case MOVE:
+                                float tx = pmx + (fx - finger.hitOnDown[0].x);
+                                float ty = pmy + (fy - finger.hitOnDown[0].y);
+                                pos(tx, ty, w() + tx, h() + ty);
+                                break;
+                            default:
+                                return s;
+                        }
+                    }
                 } else {
                     dragMode = null;
-                    return s;
                 }
+
+                return this;
+            } else {
+                potentialDragMode = dragMode = null;
+                return s;
             }
         }
+
+
         hover = false;
-        dragMode = null;
+        potentialDragMode = dragMode = null;
         return s;
     }
 
@@ -195,7 +251,23 @@ public class Windo extends Stacking {
     @Override
     protected void paint(GL2 gl) {
         if (hover && opaque()) {
-            gl.glColor3f(0.25f, 0.25f, 0.25f);
+            switch (potentialDragMode) {
+                case RESIZE_N:
+                case RESIZE_S:
+                case RESIZE_W:
+                case RESIZE_E:
+                    gl.glColor3f(0.15f, 0f, 0.2f);
+                    break;
+                case RESIZE_NE:
+                case RESIZE_SW:
+                case RESIZE_NW:
+                case RESIZE_SE:
+                    gl.glColor3f(0.15f, 0.2f, 0f);
+                    break;
+                default:
+                    gl.glColor3f(0.1f, 0.1f, 0.1f);
+                    break;
+            }
             Draw.rect(gl, x(), y(), w(), h());
         }
 
@@ -266,9 +338,11 @@ public class Windo extends Stacking {
                 )
                 , 800, 800
         );
-        //d.newWindo(new Scale(new PushButton("x"), 0.9f)).pos(10, 10, 50, 50);
-        d.newWindo(new Scale(new PushButton("x"), 0.9f)).pos(10, 10, 50, 50);;
-        d.newWindo(new Scale(new PushButton("w"), 0.9f)).pos(-50, -50, 10, 10);;
+        d.newWindo(new Scale(grid(new PushButton("x"), new PushButton("y")), 0.9f)).pos(80, 80, 250, 150);
+        d.newWindo(new Scale(new PushButton("x"), 0.9f)).pos(10, 10, 50, 50);
+        ;
+        d.newWindo(new Scale(new PushButton("w"), 0.9f)).pos(-50, -50, 10, 10);
+        ;
         //d.newWindo(new Scale(grid(new PushButton("x"), new PushButton("y")), 0.9f)).pos(-100, -100, 0, 0);
     }
 
